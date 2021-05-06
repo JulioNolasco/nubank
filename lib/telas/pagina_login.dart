fe8a682c278dbbac27908e8ac1764d04cd1e8b36
Julio Nolasco <juliocenolasco@gmail.com>
2021-05-06 18:46:59 -0300

    tela de login simples sem verificação

--- lib/telas/pagina_login.dart
+++ lib/telas/pagina_login.dart
@@ -6,64 +6,82 @@ class PaginaLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              autofocus: false,
-              decoration: InputDecoration(
-                  hintText: "email",
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(32.0),
-                  )),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextFormField(
-              autofocus: false,
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: "senha",
-                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(32.0),
-                  )),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Get.theme.primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24.0),
+      backgroundColor: Colors.purple[700],
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Stack(alignment: Alignment.center, children: [
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 200, horizontal: 20),
+              child: ListView(
+                shrinkWrap: true,
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 20),
+                children: [
+                  Text(
+                    "Faça seu login",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                ),
-                onPressed: () {
-                  Get.toNamed(Rotas.HOME);
-                },
-                child: Text(
-                  "acessar",
-                  style: TextStyle(color: Colors.white),
-                ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      hintText: "CPF",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  TextFormField(
+                    autofocus: false,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "senha",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        side: BorderSide(color: Colors.purple),
+                        shadowColor: Colors.purple,
+                        minimumSize: Size(20, 65),
+                        primary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        Get.toNamed(Rotas.HOME);
+                      },
+                      child: Text(
+                        "CONTINUAR",
+                        style: TextStyle(color: Colors.purple, fontSize: 15),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Esqueci minha senha!",
+                        style: TextStyle(color: Colors.purple),
+                      ))
+                ],
               ),
-            ),
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  "esqueceu a senha?",
-                  style: TextStyle(color: Colors.grey),
-                ))
-          ],
-        ),
+            )
+          ]),
+        ]),
       ),
     );
   }
